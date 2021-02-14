7f9538ecba452b797502116e6232cf743653a311
azureswastika <azureswastika@gmail.com>
2021-02-14 18:33:49 +0300

    Update

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from webframework import Application, BaseView
+from webframework import Application
+from webframework.views import TemplateView, View
 
 
-class Index(BaseView):
+class Index(TemplateView):
     route = "/"
-    template = "index.html"
+    template = "templates/index.html"
 
 
-class About(BaseView):
+class About(TemplateView):
     route = "/about/"
-    template = "about.html"
+    template = "templates/about.html"
+
+
+class Contacts(View):
+    route = "/contacts/"
+    template = "templates/contacts.html"
+
+    def post(self, request):
+        email = request.get("email")
+        subject = request.get("subject")
+        message = request.get("message")
+        print("Письмо от: {}\nТема: {}\nТекст: {}".format(email, subject, message))
+        return self.get(request)
 
 
 app = Application()
